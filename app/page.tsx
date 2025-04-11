a14be9e55e634600243bda2134d5897323c231d5
Mark Bradley <braddle@gmail.com>
2025-04-11 15:58:13 +0100

    fixing linting

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,37 @@
-'use client'
-import { useSearchParams } from 'next/navigation'
-import {FormEvent, useState} from "react";
+"use client";
+import { useSearchParams } from "next/navigation";
+import { FormEvent, useState } from "react";
 
 export default function Page() {
-    const searchParams = useSearchParams()
-    const [leapYear, setLeapYear] = useState<String|null>(null);
+  const searchParams = useSearchParams();
+  const [leapYear, setLeapYear] = useState<string | null>(null);
 
-    async function leapYearSubmit(event: FormEvent<HTMLFormElement>) {
-        event.preventDefault()
+  function leapYearSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
 
-        //const formData = new FormData(event.currentTarget)
+    //const formData = new FormData(event.currentTarget)
 
-        setLeapYear("is a leap year")
-    }
+    setLeapYear("is a leap year");
+  }
 
-    let content = <h2>Coming Soon!</h2>
+  let content = <h2>Coming Soon!</h2>;
 
-   if ("on" == searchParams!.get('leapYearCalculator')) {
-       content =  <div>
-           {leapYear && <h2>{leapYear}</h2>}
-           <form className="leap-year-form" onSubmit={leapYearSubmit}>
-               <input type="number" id="year_field"/>
-               <input type="submit"/>
-           </form>
-       </div>
-   }
-
-    return (
+  if ("on" == searchParams!.get("leapYearCalculator")) {
+    content = (
       <div>
-        <h1>Leap Year Calculator</h1>
-        {content}
+        {leapYear && <h2>{leapYear}</h2>}
+        <form className="leap-year-form" onSubmit={leapYearSubmit}>
+          <input type="number" id="year_field" />
+          <input type="submit" />
+        </form>
       </div>
     );
+  }
+
+  return (
+    <div>
+      <h1>Leap Year Calculator</h1>
+      {content}
+    </div>
+  );
 }
